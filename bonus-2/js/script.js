fe0a438f131f08aca7ea21ad73a4a769f19016d8
NicolaVerlato <nicola.verlato00@gmail.com>
2022-06-24 16:46:16 +0200

    finito bonus 2

---
+++ bonus-2/js/script.js
@@ -0,0 +1,62 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            currentSlide: 0,
+            start: '',
+            slides: [
+                {
+                    image: 'img/01.jpg',
+                    title: 'Svezia',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                },
+                {
+                    image: 'img/02.jpg',
+                    title: 'Svizzera',
+                    text: 'Lorem ipsum.',
+                },
+                {
+                    image: 'img/03.jpg',
+                    title: 'Gran Bretagna',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                },
+                {
+                    image: 'img/04.jpg',
+                    title: 'Germania',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                },
+                {
+                    image: 'img/05.jpg',
+                    title: 'Paradise',
+                    text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+                }
+            ],
+        },
+        methods: {
+            nextSlide() {
+                if(this.currentSlide < this.slides.length - 1){
+                    this.currentSlide++;
+                    
+                } else{
+                    this.currentSlide = 0;
+                }
+            },
+            previousSlide() {
+                if(this.currentSlide > 0){
+                    this.currentSlide--;
+                } else{
+                    this.currentSlide = this.slides.length - 1; 
+                }
+            },
+            setActive(index) {
+                this.currentSlide = index;
+            },
+            startChanging(){
+                this.start = setInterval(this.nextSlide, 3000);
+            },
+        },
+        mounted() {
+            this.startChanging();
+        }
+    }
+)
